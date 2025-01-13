d6ed616fff52531bf2174f8c72850f4d938b6c28
Dev <aot.dev@gmail.com>
2025-01-13 23:36:36 +0530

    .env

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import LookupForm from './components/LookupForm';
 import LookupResult from './components/LookupResult';
 
+const apiUrl = import.meta.env.API_URL;
 const App = () => {
   const [result, setResult] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
@@ -15,7 +16,7 @@ const App = () => {
     setResult('');
 
     try {
-      const response = await axios.post(`http://13.61.186.57:8080/api/lookup`,{
+      const response = await axios.post(`${apiUrl}lookup`,{
         query
       });
       setResult(response.data);
