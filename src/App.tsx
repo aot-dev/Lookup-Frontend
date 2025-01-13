50c3348654c19b6859719c284c5bf805745f08b9
Dev <aot.dev@gmail.com>
2025-01-13 21:34:20 +0530

    backend

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
     setResult('');
 
     try {
-      const response = await axios.post(`http://localhost:3001/api/lookup`,{
+      const response = await axios.post(`http://13.61.186.57:80/api/lookup`,{
         query
       });
       setResult(response.data);
